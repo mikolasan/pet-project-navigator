2881b010394d7ac4fc791af3a9e84a8dd5683115
Nikolay Neupokoev <neupokoev.n@gmail.com>
2016-08-23 22:08:20 -0700

    add new activity: page where you create new project

--- app/src/main/java/io/github/mikolasan/petprojectnavigator/MainActivity.java
+++ app/src/main/java/io/github/mikolasan/petprojectnavigator/MainActivity.java
@@ -1,7 +1,10 @@
 package io.github.mikolasan.petprojectnavigator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btn_add_project = (Button) findViewById(R.id.btn_add_project);
+        btn_add_project.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProjectActivity.class);
+                intent.putExtra("status", "new");
+                startActivity(intent);
+            }
+        });
+
     }
 }
