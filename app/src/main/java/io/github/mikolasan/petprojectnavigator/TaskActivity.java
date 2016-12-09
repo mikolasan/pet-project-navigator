c857e45097224bc7488a157afdddc3815176c6c2
Nikolay Neupokoev <neupokoev.n@gmail.com>
2016-12-08 21:50:04 -0800

    Add dialog to add a new technology
    refs #2

--- app/src/main/java/io/github/mikolasan/petprojectnavigator/TaskActivity.java
+++ app/src/main/java/io/github/mikolasan/petprojectnavigator/TaskActivity.java
@@ -1,17 +1,27 @@
 package io.github.mikolasan.petprojectnavigator;
 
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.widget.SimpleCursorAdapter;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 
-public class TaskActivity extends AppCompatActivity {
+interface MyListener {
+    // you can define any parameter as per your requirement
+    public void callback(View view, String result);
+}
+
+public class TaskActivity extends FragmentActivity implements MyListener {
 
     public static final int STATUS_NEW = 0;
     public static final int STATUS_EDIT = 1;
@@ -25,6 +35,41 @@ public class TaskActivity extends AppCompatActivity {
 
     SimpleCursorAdapter spinnerAdapter;
     private Spinner s_tech;
+
+    FragmentTransaction ft;
+
+    private void removeTechDialog() {
+        ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("TechNameDialogFragment");
+        if (prev != null) {
+            ((DialogFragment)prev).dismiss();
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+    }
+    private void openTechDialog() {
+        TechNameDialogFragment dialog = TechNameDialogFragment.newInstance(TaskActivity.this);
+        dialog.show(ft,"TechNameDialogFragment");
+    }
+
+    private void updateSpinner() {
+        if (spinnerAdapter != null) {
+            spinnerAdapter.swapCursor(db.getAllTech());
+        }
+    }
+
+    private void selectItem(String result) {
+        Cursor c = spinnerAdapter.getCursor();
+        int id = 0;
+        if (c.moveToLast()) {
+            id = c.getPosition();
+        }
+        int name_id = c.getColumnIndex(DB.COLUMN_NAME);
+        String label = c.getString(name_id);
+        if(label.equals(result)) {
+            s_tech.setSelection(id);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,8 +122,15 @@ public class TaskActivity extends AppCompatActivity {
                 Cursor cursor = (Cursor) parentView.getItemAtPosition(position);
                 int index = cursor.getColumnIndex(DB.COLUMN_ID);
                 int tech_id = cursor.getInt(index);
-                if (tech_id == DB.TECH_UNDEFINED_ID) {
-                    
+                switch (tech_id) {
+                    case DB.TECH_UNDEFINED_ID: {
+                        break;
+                    }
+                    case DB.TECH_NEW_ID: {
+                        removeTechDialog();
+                        openTechDialog();
+                        break;
+                    }
                 }
 
             }
@@ -108,5 +160,39 @@ public class TaskActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void callback(View view, String result) {
+        db.addTech(result);
+        removeTechDialog();
+        updateSpinner();
+        selectItem(result);
+    }
+
+
+    public static class TechNameDialogFragment extends DialogFragment {
+        MyListener ml;
+
+        static TechNameDialogFragment newInstance(MyListener ml) {
+            TechNameDialogFragment f = new TechNameDialogFragment();
+            f.ml = ml;
+            return f;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                   Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.dialog_tech, container, false);
+            final EditText e = (EditText)v.findViewById(R.id.e_tech_name);
+            // Watch for button clicks.
+            Button button = (Button)v.findViewById(R.id.btn_add_tech);
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    ml.callback(e, e.getText().toString());
+
+                }
+            });
+            return v;
+        }
+    }
 
 }
