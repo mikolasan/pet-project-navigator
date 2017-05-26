133448e11de462716e8c30cb1d008fbcd5df8b45
Nikolay Neupokoev <neupokoev.n@gmail.com>
2017-05-25 22:52:13 -0700

    Add Toolbar on task page
    refs #7

--- app/src/main/java/io/github/mikolasan/petprojectnavigator/TaskActivity.java
+++ app/src/main/java/io/github/mikolasan/petprojectnavigator/TaskActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -277,6 +278,14 @@ public class TaskActivity extends AppCompatActivity implements PetDialogListener
                 break;
             }
         }
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.task_toolbar, menu);
+        return true;
     }
 
     @Override
