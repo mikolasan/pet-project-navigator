bdf53e6bd3ae531da88db17b465a225006762dd7
Nikolay Neupokoev <neupokoev.n@gmail.com>
2017-05-29 22:51:09 -0700

    Fill task data in ListView
    Set dark theme again
    
    refs #17

--- app/src/main/java/io/github/mikolasan/petprojectnavigator/PetTaskLoader.java
+++ app/src/main/java/io/github/mikolasan/petprojectnavigator/PetTaskLoader.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.View;
+import android.widget.TextView;
 
 class PetTaskLoader extends PetAnyLoader {
     public PetTaskLoader(Context context, PetDatabase petDatabase) {
@@ -30,7 +32,23 @@ class PetTaskLoader extends PetAnyLoader {
         }
     }
 
-    CursorAdapter createAdapter(Cursor cursor) {
-        return new SimpleCursorAdapter(getContext(), getLayoutId(), null, getColumnNames(), getLayoutItems(), 0);
+    @Override
+    protected void fillView(View view, Context context, Cursor cursor) {
+        int totalTime = cursor.getInt(cursor.getColumnIndex(DBTask.COLUMN_TIME));
+        String time = String.valueOf(totalTime) + " hour";
+        if (totalTime > 1) time += "s";
+
+        int techId = cursor.getInt(cursor.getColumnIndex(DBTask.COLUMN_TECH_ID));
+        String technology = petDatabase.getTechName(techId) + ";";
+
+        ((TextView) view.findViewById(R.id.lbl_title)).setText(
+                cursor.getString(cursor.getColumnIndex(PetDatabase.COLUMN_NAME))
+        );
+        ((TextView) view.findViewById(R.id.lbl_time)).setText(
+                time
+        );
+        ((TextView) view.findViewById(R.id.lbl_tech)).setText(
+                technology
+        );
     }
 }
